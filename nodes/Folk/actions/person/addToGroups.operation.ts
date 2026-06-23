8c713a59773aa3ec09db31043bda60fefcbb496c
akateeterclaw <akateeterclaw@gmail.com>
2026-06-23 14:55:12 -0600

    Add Add to Groups person operation

---
+++ nodes/Folk/actions/person/addToGroups.operation.ts
@@ -0,0 +1,104 @@
+import type {
+	IDataObject,
+	IExecuteSingleFunctions,
+	IHttpRequestOptions,
+	INodeProperties,
+} from 'n8n-workflow';
+
+const displayOptions = {
+	show: {
+		resource: ['person'],
+		operation: ['addToGroups'],
+	},
+};
+
+interface FolkPersonResponse {
+	data?: {
+		groups?: Array<{
+			id: string;
+		}>;
+	};
+}
+
+async function mergeGroups(
+	this: IExecuteSingleFunctions,
+	requestOptions: IHttpRequestOptions,
+): Promise<IHttpRequestOptions> {
+	const personId = this.getNodeParameter('personId') as string;
+	const addGroups = this.getNodeParameter('addGroups') as {
+		groupValues?: Array<{ id: string }>;
+	};
+	const response = (await this.helpers.httpRequestWithAuthentication.call(
+		this,
+		'folkApi',
+		{
+			method: 'GET',
+			url: `https://api.folk.app/v1/people/${personId}`,
+		},
+	)) as FolkPersonResponse;
+	const groupsById = new Map<string, { id: string }>();
+
+	for (const group of response.data?.groups ?? []) {
+		groupsById.set(group.id, { id: group.id });
+	}
+
+	for (const group of addGroups.groupValues ?? []) {
+		if (group.id) {
+			groupsById.set(group.id, { id: group.id });
+		}
+	}
+
+	const body = (requestOptions.body ?? {}) as IDataObject;
+	body.groups = Array.from(groupsById.values());
+	requestOptions.body = body;
+
+	return requestOptions;
+}
+
+export const addToGroupsDescription: INodeProperties[] = [
+	{
+		displayName: 'Person ID',
+		name: 'personId',
+		type: 'string',
+		default: '',
+		required: true,
+		displayOptions,
+		description: 'The ID of the person to add to groups',
+	},
+	{
+		displayName: 'Groups',
+		name: 'addGroups',
+		type: 'fixedCollection',
+		typeOptions: {
+			multipleValues: true,
+		},
+		default: {},
+		required: true,
+		displayOptions,
+		description: 'Groups to add without removing the person from existing groups',
+		options: [
+			{
+				displayName: 'Group',
+				name: 'groupValues',
+				values: [
+					{
+						displayName: 'Group ID',
+						name: 'id',
+						type: 'string',
+						default: '',
+						required: true,
+						description: 'The ID of the group to add',
+					},
+				],
+			},
+		],
+		routing: {
+			send: {
+				type: 'body',
+				property: 'groups',
+				value: '={{ $value.groupValues?.map(g => ({ id: g.id })) || [] }}',
+				preSend: [mergeGroups],
+			},
+		},
+	},
+];
